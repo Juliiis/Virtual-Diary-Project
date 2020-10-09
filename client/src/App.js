20817447b6c525ea9d9285691976540e52a894cc
julieta <jumartinnob@gmail.com>
2020-10-09 19:46:46 +0200

    Update node-forge

--- client/src/App.js
+++ client/src/App.js
@@ -115,40 +115,42 @@ class App extends React.Component {
    const { text, date, information } = this.state;
   return (
     <div className="container">
-        <h2>MY VIRTUAL DIARY</h2>
-          {/* <div>{text}</div>  */}
+      <div className="row">
+        <div className="col-3">
+          <h2>MY VIRTUAL DIARY</h2>
+        </div>
+              {/* <div>{text}</div>  */}
+        <div className="col-6">
           <textarea
               type="text"
               value={text}
               onChange={this.handleInput}
               placeholder="How was your day?.."
               maxLength="500"
-              className="col-sm-4"
             ></textarea>
-       <div>
+       
     
-        <div style={{ fontSize: 10, color: "red"}}>
-          {this.state.textError}          
+          <div style={{ fontSize: 10, color: "red"}}>
+            {this.state.textError}          
+          </div>
+
+          <input 
+          type="date" 
+          value={date} 
+          onChange={this.dateAgost} 
+          placeholder="00/00/0000" 
+          /> 
+        
+          {this.state.showError ? (<div className="text-danger"><i>All inputs are required</i></div>) : (<div></div>)}
+        
+          <button onClick={e => this.addDay()} className="btn btn-outline-secondary btn-sm "><b>SAVE IN DATABASE</b></button>
         </div>
-
-        <input 
-        type="date" 
-        value={date} 
-        onChange={this.dateAgost} 
-        placeholder="00/00/0000" 
-        /> <br></br><br></br>
-        
-        {this.state.showError ? (<div className="text-danger"><i>All inputs are required</i></div>) : (<div></div>)}<br></br>
-        
-        <button onClick={e => this.addDay()} className="btn btn-outline-secondary btn-sm "><b>SAVE IN DATABASE</b></button><br></br>
-        
-       </div>
-
-      <div className="col-3">
+     
+          <div className="col-3">
             {information.map(each => {
               return (
                 <div key={each.id} >
-                  <div><button onClick={() => this.viewText(each.text)} className="btn btn-outline-secondary btn-sm float-left ">
+                  <div><button onClick={() => this.viewText(each.text)} className="btn btn-outline-secondary btn-sm">
                     {each.date}
                   </button></div>
                   <button onClick={() => this.deleteDate(each.id)} className="btn"><i class="far fa-trash-alt"></i>
@@ -156,7 +158,8 @@ class App extends React.Component {
                 </div>
               );
             })}
-      </div>
+          </div>
+        </div>
       </div>
        
 
